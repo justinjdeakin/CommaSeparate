b70b35e6b60408cb45e5d1a4dee3ec22a13cc04b
Justin <justin.john@deakin.edu.au>
2025-07-17 16:03:25 +1000

    Add runner to test program

---
+++ TempRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

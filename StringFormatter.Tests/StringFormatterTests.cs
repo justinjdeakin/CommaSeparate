aa054b5c9cc3e6d4e0ad07164f5cc6cbfaa19a9a
Justin <justin.john@deakin.edu.au>
2025-07-18 10:14:03 +1000

    Generate boiler for testing

---
+++ StringFormatter.Tests/StringFormatterTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StringFormatter.Tests
+{
+    public class StringFormatterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
